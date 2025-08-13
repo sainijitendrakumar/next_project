d9eb8adbc93f8a895a8d0fb61070f599600a8c83
jitendra <er.sainijitendra@gmail.com>
2025-08-13 17:17:26 +0530

    add some typo

--- src/Pages/Movie_profile/Movieprofile.jsx
+++ src/Pages/Movie_profile/Movieprofile.jsx
@@ -52,11 +52,11 @@ function dataprofile() {
           data.length !== 0 &&
           data.map((movie) => (
             <div className="flex flex-col items-center" key={movie.title}>
-              <h1 className="text-3xl font-bold py-2 ">
+              <h1 className="text-2xl sm:text-3xl font-bold py-2 text-center ">
                 Download | {movie.title} | Full data 720p
               </h1>
 
-              <p className="text-xl font-semibold text-center py-2">
+              <p className="sm:text-xl font-semibold text-center py-2">
                 {movie.discription}
               </p>
               <img
@@ -71,13 +71,13 @@ function dataprofile() {
                   margin: "5px",
                 }}
               />
-              <h2 className="font-bold text-4xl text-gray-800 text-center">
+              <h2 className="font-bold text-2xl sm:text-4xl text-gray-800 text-center">
                 Download | {movie.title} | {movie.language} | Full data 720p
               </h2>
-              <p className="font-bold text-3xl my-3">
+              <p className="font-bold text-xl sm:text-3xl my-3 text-center">
                 Click on the Below Download Button to Download file
               </p>
-              <h3 className="text-red-700 text-2xl font-bold my-2">
+              <h3 className="text-red-700 text:xl sm:text-2xl font-bold my-2 text-center">
                 {" "}
                 : DOWNLOADS LINKS :{" "}
               </h3>
@@ -85,13 +85,13 @@ function dataprofile() {
                 -: {movie.title} :-
               </h4>
               <p className="font-[12px] text-[#aaa]  ">
-                <span className="bg-[#FF6D00] text-white font-bold text-[20px] px-[38px] py-[19px] rounded-[7px] inline-block text-center m-[4px_2px]">
+                <span className="bg-[#FF6D00] text-white font-bold text-[15px] sm:text-[20px] px-[38px] py-[19px] rounded-[7px] inline-block text-center m-[4px_2px]">
                   G-Drive [GDToT] Links:
                 </span>
               </p>
-              <div className="w-2/3 my-2.5 p-1 text-white bg-[#7d3741] border text-center border-red-500 rounded-3xl">
+              <div className="sm:w-2/3 my-2.5 p-1 text-white bg-[#7d3741] border text-center border-red-500 rounded-3xl">
                 <a
-                  className=" text-xl font-semibold"
+                  className="text-[13px] sm:text-xl p-2 font-semibold"
                   href={movie.movie_drive_url}
                   target="_blank"
                   rel="noopener noreferrer"
@@ -100,13 +100,13 @@ function dataprofile() {
                 </a>
               </div>
               <p className="font-[12px] text-[#aaa]  ">
-                <span className="bg-[#FF6D00] text-white font-bold text-[20px] px-[38px] py-[19px] rounded-[7px] inline-block text-center m-[4px_2px]">
+                <span className="bg-[#FF6D00] text-white font-bold text-[15px] sm:text-[20px] px-[38px] py-[19px] rounded-[7px] inline-block text-center m-[4px_2px]">
                   Telegram Links:
                 </span>
               </p>
-              <div className="w-2/3 my-2.5 p-1 text-white bg-[#7d3741] border text-center border-red-500 rounded-3xl">
+              <div className="sm:w-2/3 my-2.5 p-1 text-white bg-[#7d3741] border text-center border-red-500 rounded-3xl">
                 <a
-                  className=" text-xl font-semibold"
+                  className=" text-[13px] sm:text-xl p-2 font-semibold"
                   href={movie.telegram_url}
                   target="_blank"
                   rel="noopener noreferrer"
