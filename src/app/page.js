b34976f79921ee0b9995898098f47eade0720cce
jitendra <er.sainijitendra@gmail.com>
2025-08-14 21:16:18 +0530

    add some typo

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
 
-import Navbar from "../Components/Navbar/Navbar";
-import Image from "next/image";
-import Footer from "../Components/Footer/Footer";
 import Home from "@/Pages/Home/Home";
-import AboutSection from "@/Pages/AboutUs/AboutSection";
 
 export default function Page() {
   return (
