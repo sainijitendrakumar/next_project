460ea82966fa5fd5ede0742fb84470c125dd0e66
jitendra <er.sainijitendra@gmail.com>
2025-08-16 20:07:12 +0530

    add some typo

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,14 +31,6 @@ export default function RootLayout({ children }) {
       ><Socialbar />
         <Popunder />
         <Navbar />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-X4PY6D6N29"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
-
-  gtag('config', 'G-X4PY6D6N29');
-</script>
         {children}
         <Footer />
       </body>
