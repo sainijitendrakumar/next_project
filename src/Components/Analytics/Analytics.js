d16c117b3e2da8aa422cdda5915ca73bc608674a
jitendra <er.sainijitendra@gmail.com>
2025-08-17 19:36:00 +0530

    add google analitics

--- src/Components/Analytics/Analytics.js
+++ src/Components/Analytics/Analytics.js
@@ -1,31 +1,17 @@
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 import * as gtag from "../../app/lib/gtag";
 
 export default function Analytics() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    // Listen for GA script load
-    const timer = setInterval(() => {
-      if (typeof window.gtag !== "undefined") {
-        setLoaded(true);
-        clearInterval(timer);
-      }
-    }, 300);
-    return () => clearInterval(timer);
-  }, []);
-
-  useEffect(() => {
-    if (!loaded || !pathname) return;
-
-    const url = pathname + (searchParams.toString() ? `?${searchParams}` : "");
-    gtag.pageview(url);
-  }, [loaded, pathname, searchParams]);
+    if (pathname) {
+      gtag.pageview(pathname);
+    }
+  }, [pathname]);
 
   return null;
 }
