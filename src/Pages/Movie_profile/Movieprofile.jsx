ef2df03c6993188a6e915aea70359adf9f95dd29
jitendra <er.sainijitendra@gmail.com>
2025-08-18 20:23:22 +0530

    add some typos

--- src/Pages/Movie_profile/Movieprofile.jsx
+++ src/Pages/Movie_profile/Movieprofile.jsx
@@ -104,7 +104,7 @@ function dataprofile() {
               <div className="sm:w-2/3 my-2.5 p-1 text-white bg-[#7d3741] border text-center border-red-500 rounded-3xl">
                 <a
                   className=" text-[13px] sm:text-xl p-2 font-semibold"
-                  href={movie.telegram_url}
+                  href="https://t.me/+bFS7JDcUoa81Y2I1"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
