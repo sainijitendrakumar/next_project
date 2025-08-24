21531b05805b4b00870c05c8bc5e1d16020857ae
jitendra <er.sainijitendra@gmail.com>
2025-08-24 17:00:02 +0530

    add some typos

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 import Home from "@/Pages/Home/Home";
 export const metadata = {
   title: "SeeHDmovie: Movie Download",
-  description: "Download the latest Hollywood, Bollywood, and South Indian movies in HD quality. Fast and free movie download, no registration required.only on  seehdmovie",
-   keywords:"HD movie download, latest Movies, movie download site" ,
+  description: "Download the latest Hollywood, Bollywood, and South Indian movies in HD quality. Fast and free movie download, no registration required. only on seehdmovie",
+   keywords:"HD movie download, latest Movies, movie download site, seehdmovie",
     canonical:"https://seehdmovie.xyz/"
 };
 
