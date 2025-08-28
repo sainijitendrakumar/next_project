fa6cda3f94bd573c2384012bfd0d45a689e9f2b5
jitendra <er.sainijitendra@gmail.com>
2025-08-28 12:36:56 +0530

    add some typos

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Home from "@/Pages/Home/Home";
 export const metadata = {
-  title: "SeeHDmovie: Movies Download",
+  title: "SeeHDmovie - Movies Download",
   description: "Download the latest Hollywood, Bollywood, and South Indian movies in HD quality. Fast and free movie download, no registration required. only on seehdmovie",
    keywords:"HD movie, download, seehd, latest Movies, seehdmovies",
     canonical:"https://seehdmovie.xyz/"
@@ -10,7 +10,7 @@ export default function Page() {
   return (
     <>
     <h1 className="font-bold font-sans text-2xl sm:text-3xl text-center text-red-600 my-4">
-        Free HD Movie Download | Bollywood, Hollywood & Dual Audio Movies - SeeHDMovie
+        Free HD Movies Download | Bollywood, Hollywood & Dual Audio Movies - SeeHDMovie
       </h1>
      <Home />
     </>
