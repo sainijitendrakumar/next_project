8fedcf62e2e6850cfda64b824f695156aa207a5c
jitendra <er.sainijitendra@gmail.com>
2025-09-03 17:06:19 +0530

    add some typos

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
 
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    crossOrigin: 'anonymous',
+}
+ 
 export default nextConfig;
