875cb9d60c235e64ff17ec3a44588d50387daaf1
jitendra <er.sainijitendra@gmail.com>
2025-09-04 20:56:00 +0530

    add some typos

--- src/app/category/hollywood-movies/page.js
+++ src/app/category/hollywood-movies/page.js
@@ -1,7 +1,7 @@
 import Hollywood from "../../../Pages/Category/Hollywood/HollywoodMovies"
 export const metadata = {
-    title: "Download Latest Hollywood HD Movies Free - SeeHDMovie",
-  description: "Download the latest Hollywood movies in free HD—stream or download dual-audio and high-definition films at SeeHDMovie.",
+    title: "Filmyzilla Alternative to Watch Hindi Dubbed Hollywood Movies Online",
+  description: "Looking for a Filmyzilla alternative? On seehdmovie.xyz you can watch and stream Hindi dubbed Hollywood movies online in HD quality for free.",
         
   keywords:"HD movie download, Bollywood movies, Hollywood movies, Dual audio movies, 720p movies, 1080p movies, full movie download, free movie download, Hindi dubbed movies, South Indian movies, movie download site, latest movies 2025, seehdmovie, seehdmovie.xyz",
   canonical:"https://seehdmovie.xyz/category/hollywood-movies"
@@ -11,7 +11,7 @@ export default function Page() {
   return (
     <>
     <h1 className="font-bold font-sans text-2xl sm:text-3xl text-center text-red-600 my-4">
-      Latest Hollywood Free HD Movie Download | Dual Audio Movies - SeeHDMovie
+      Best Filmyzilla Alternative for Hindi Dubbed Hollywood Movies | Dual Audio Movies - SeeHDMovie
       </h1>
       <Hollywood />
     </>
