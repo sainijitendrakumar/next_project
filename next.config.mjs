63e488b083c4279f390733d8d4f81253cda39146
jitendra <er.sainijitendra@gmail.com>
2025-09-05 13:32:00 +0530

    add some typos

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     crossOrigin: 'anonymous',
+    async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.seehdmovie.xyz' }],
+        destination: 'https://seehdmovie.xyz/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
  
 export default nextConfig;
