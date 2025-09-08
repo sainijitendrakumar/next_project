182d2c5ce6ff0929847e531618f61a04f346792a
jitendra <er.sainijitendra@gmail.com>
2025-09-08 18:14:59 +0530

    add some typos

--- src/app/page.js
+++ src/app/page.js
@@ -10,9 +10,18 @@ export const metadata = {
 export default function Page() {
   return (
     <>
-    <Head>
-      <link rel="canonical" href="https://seehdmovie.xyz/" />
-    </Head>
+      <Head>
+        <title>SeeHDmovie - Movies Download</title>
+        <meta
+          name="description"
+          content="Download the latest Hollywood, Bollywood, and South Indian movies in HD quality. Fast and free movie download, no registration required. Only on SeeHDmovie."
+        />
+        <meta
+          name="keywords"
+          content="HD movie, download, seehd, latest Movies, seehdmovies"
+        />
+        <link rel="canonical" href="https://seehdmovie.xyz/" />
+      </Head>
     <h1 className="font-bold font-sans text-2xl sm:text-3xl text-center text-red-600 my-4">
         Free HD Movies Download | Bollywood, Hollywood & Dual Audio Movies - SeeHDMovie
       </h1>
