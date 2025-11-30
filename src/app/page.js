82768c08a9fee80b5d690641f940a07ff4320508
jitendra <er.sainijitendra@gmail.com>
2025-11-30 18:07:25 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 import Home from "@/Pages/Home/Home";
 import Head from "next/head";
 export const metadata = {
-  title: "SeeHDmovie - Movies Download",
-  description: "Download the latest Hollywood, Bollywood, and South Indian movies in HD quality. Fast and free movie download, no registration required. only on seehdmovie",
-   keywords:"HD movie, download, seehd, latest Movies, seehdmovies",
+  title: "Mkvcinemas.com - Movies Download",
+  description: "Download the latest Hollywood, Bollywood, and South Indian movies in HD quality. Fast and free movie download - Mkvcinemas",
+   keywords:"HD movie, download, seehd, latest Movies, Mkvcinemas",
     canonical:"https://seehdmovie.xyz/"
 };
 
@@ -11,7 +11,7 @@ export default function Page() {
   return (
     <>
       <Head>
-        <title>SeeHDmovie - Movies Download</title>
+        <title>Mkvcinemas.com - Movies Download</title>
         <meta
           name="description"
           content="Download the latest Hollywood, Bollywood, and South Indian movies in HD quality. Fast and free movie download, no registration required. Only on SeeHDmovie."
@@ -23,7 +23,7 @@ export default function Page() {
         <link rel="canonical" href="https://seehdmovie.xyz/" />
       </Head>
     <h1 className="font-bold font-sans text-2xl sm:text-3xl text-center text-red-600 my-4">
-        Free HD Movies Download | Bollywood, Hollywood & Dual Audio Movies - SeeHDMovie
+      Mkvcinemas | Bollywood, Hollywood & Dual Audio Movies - mkvcinemas
       </h1>
      <Home />
     </>
