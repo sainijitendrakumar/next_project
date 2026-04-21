70550962914899a69fb9e7cbe705a46e00441938
jitendra <er.sainijitendra@gmail.com>
2026-04-21 19:42:51 +0530

    first commit

--- src/Components/Adsterra/Popunder/Popunder.jsx
+++ src/Components/Adsterra/Popunder/Popunder.jsx
@@ -5,7 +5,7 @@ function Popunder() {
   useEffect(() => {
     const script = document.createElement("script");
     script.src =
-      "//pl27312544.profitableratecpm.com/0e/9b/9b/0e9b9b14fff92f3a1445a9a3608c3af5.js";
+      "//pl27312544.profitablecpmratenetwork.com/0e/9b/9b/0e9b9b14fff92f3a1445a9a3608c3af5.js";
     script.type = "text/javascript";
     script.async = true;
     document.body.appendChild(script);
