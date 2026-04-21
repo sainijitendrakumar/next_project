00e743dac77f7d98fc4211bc837d40f116fb37fa
jitendra <er.sainijitendra@gmail.com>
2026-04-21 19:52:36 +0530

    first commit

--- src/Components/Adsterra/Socialbar/Socialbar.jsx
+++ src/Components/Adsterra/Socialbar/Socialbar.jsx
@@ -5,7 +5,7 @@ function Socialbar() {
   useEffect(() => {
     const script = document.createElement("script");
     script.src =
-      "//pl27312553.profitableratecpm.com/b0/8b/40/b08b40ee3f8cdadaadcf1f931068038b.js";
+      "//pl27312553.profitablecpmratenetwork.com/b0/8b/40/b08b40ee3f8cdadaadcf1f931068038b.js";
     script.type = "text/javascript";
     script.async = true;
     document.body.appendChild(script);
